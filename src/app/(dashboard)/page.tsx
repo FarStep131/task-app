e7156d245f49269415569ecb4ee18231e034adb0
naoki-yazawa <naoki.yazawa@dena.com>
2024-03-18 14:15:34 +0900

    feat: create Task list page

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,3 +1,32 @@
-export default function Page() {
-  return <>Task Dashboard</>;
+import { Metadata } from "next";
+import { getAuthSession } from "@/lib/auth";
+import { getClient } from "@/lib/apollo-client";
+import { SelectTasksByUserIdDocument } from "@/graphql/generated/gql.types";
+import { columns } from "./_components/columns";
+import { DataTable } from "./_components/data-table";
+import { redirect } from "next/navigation";
+
+export const metadata: Metadata = {
+  title: "Tasks",
+  description: "A task and issue tracker build using Tanstack Table.",
+};
+
+export default async function TaskPage() {
+  const session = await getAuthSession();
+  if (!session) redirect("/sign_in");
+
+  const {
+    data: { tasks },
+  } = await getClient().query({
+    query: SelectTasksByUserIdDocument,
+    variables: {
+      user_id: session.user.id,
+    },
+  });
+
+  return (
+    <div className="h-full my-5">
+      <DataTable data={tasks} columns={columns} session={session} />
+    </div>
+  );
 }
